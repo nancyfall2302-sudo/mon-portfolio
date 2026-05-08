3786cf3c1554e305d54a02e24f80fd1f845d2881
nancyfall2302-sudo <nancyfall2302@gmail.com>
2026-05-08 11:39:47 +0000

    Add files via upload

---
+++ resume.py
@@ -0,0 +1,61 @@
+import streamlit as st
+
+st.set_page_config(page_title="Portfolio de Nancy Jupiter FALL", layout="wide")
+
+#entete----------------------------------------------------------------------------------------
+
+st.title("Nancy Jupiter FALL")
+
+st.subheader("Technicienne Supérieure en Sciences et Techniques Agricole, Alimentaire et Nutritionnelle")
+
+st.write("_________________________________________________________________________________________________________")
+
+
+#barre de coté------------------------------------------------------------------------
+
+with st.sidebar:
+    st.header("Education")
+    st.markdown("""
+* 2024-2027 : Licence en sciences & techniques agricole, alimentaire et nutritionnelle à l’Université Amadou Mahtar Mbow, Diamniadio, Dakar
+
+* 2023-2024 : Baccalauréat scientifique, Dakar
+
+""")
+    st.write("________________________________")
+    st.header("Contact")
+    st.write("📧 nancyfall2302@gmail.com")
+    st.write("📍 Dakar, Sénégal")
+    st.write("________________________________")
+    st.write("Disponible et joignable par mail pour toute collaboration ou échange professionnel")
+
+
+
+
+#contenuuuuuuuuuuuuuuuuuuuuuuu----------------------------------------------------------------------------------------
+
+st.subheader("Profil")
+
+st.write("Technicienne supérieure motivée, passionnée par l'agriculture durable et la transformation alimentaire. Dotée de capacités d'analyse et de résolution de problèmes et d'expérience pratique en laboratoire. A la recherche d'opportunités pour acquérir de l'expérience professionnelle.")
+
+st.subheader("Expériences Pratiques")
+
+st.markdown("""
+* Irrigation, entretien et suivi de cultures expérimentales
+* Réalisation de cultures et analyses microbiologiques
+* Préparation et observation d'échantillons animales et végétales au microscope
+* Manipulation d'équipements de laboratoire
+* Rédaction de comptes rendus scientifiques
+""")
+
+st.subheader("Compétences")
+
+st.markdown("""
+* Techniques de base d'analyse alimentaire
+* Collecte et analyse de données (enquêtes, analyses de laboratoire)
+* Pratiques agricoles de base
+* Élaboration de budgets de base (Excel)
+* Microsoft office, OTC Chrome et Canva
+* Aptitude à la communication et au travail d'équipe
+""")
+
+
